c208cdea61012ab7de0b7978a1fa3c851f02c563
Ian Cohen <ian_cohen90@live.com>
2021-07-09 16:44:38 -0400

    added confirms for different options plus added a new empty array that will add each array that user wants for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 var number = ["0", "1", "2","3", "4","5", "6", "7", "8", "9", "0"];
 var lowercase = ["a", "b", "c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upppercase = ["A", "B", "C","D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var uppercase = ["A", "B", "C","D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "<", ">"];
 
 // Get references to the #generate element
@@ -14,6 +14,7 @@ var confirmNumber;
 var confirmUppercase;
 var confirmLowercase;
 
+// Starts the generatePassword function to prompt user how many characters they would like
 function generatePassword(){
   var confirmLength = prompt("How many characters would you like your password to contain?");
 
@@ -23,6 +24,24 @@ function generatePassword(){
     var confirmUppercase = confirm("Click OK to confirm if you would like to include uppercase letters");
     var confirmLowercase = confirm("Click OK to confirm if you would like to include lowercase letters");
 
+
+    //creates an empty array. If statements take each single array and creates one array called passwordCharacters
+    var passwordCharacters = [];
+
+    if(confirmSpecial){
+      passwordCharacters = passwordCharacters.concat(special);
+    }
+    if(confirmNumber){
+      passwordCharacters = passwordCharacters.concat(number);
+    }
+    if(confirmUppercase){
+      passwordCharacters = passwordCharacters.concat(uppercase);
+    }
+    if(confirmLowercase){
+      passwordCharacters = passwordCharacters.concat(lowercase);
+    }
+
+    console.log(passwordCharacters);
   }else{
     alert("Password length must be between 8-128 characters. Try Again!");
     var confirmLength = prompt("How many characters would you like your password to contain?");
